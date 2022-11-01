59f738da9b4995ae4950e3942de115d26f049cad
Carsten Hauge <chn_ext@cookieinformation.com>
2022-11-01 10:27:29 +0100

    MCS-4 - Updating ChuckerCollector instantiation and removing unused use of https://plugins.gradle.org/plugin/io.gitlab.arturbosch.detekt

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,8 @@
+/*
 repositories {
   gradlePluginPortal()
 }
+*/
 
 /*
 plugins {
--- sample/src/debug/java/com/cookieinformation/mobileconsents/sample/OkHttpClientProvider.kt
+++ sample/src/debug/java/com/cookieinformation/mobileconsents/sample/OkHttpClientProvider.kt
@@ -1,7 +1,15 @@
 package com.cookieinformation.mobileconsents.sample
 
 import android.content.Context
-import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor.Builder
 import okhttp3.OkHttpClient
 
-fun getOkHttpClient(context: Context) = OkHttpClient.Builder().addInterceptor(ChuckerInterceptor(context)).build()
+fun getOkHttpClient(context: Context) = OkHttpClient.Builder().addInterceptor(
+  Builder(context)
+    .collector(ChuckerCollector(context))
+    .maxContentLength(250000L)
+    .redactHeaders(emptySet())
+    .alwaysReadResponseBody(false)
+    .build()
+).build()
