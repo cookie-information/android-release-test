c3c278787ee77d747903637d9c48bb92c3599c67
Carsten Hauge <chn_ext@cookieinformation.com>
2022-12-08 10:23:41 +0100

    MCS-17 Removed the styles of privacy center

--- sample/src/main/java/com/cookieinformation/mobileconsents/sample/MainFragment.kt
+++ sample/src/main/java/com/cookieinformation/mobileconsents/sample/MainFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_main.buttonFetchSend
-import kotlinx.android.synthetic.main.fragment_main.buttonPrivacyCenter
 import kotlinx.android.synthetic.main.fragment_main.buttonPrivacyFragment
 import kotlinx.android.synthetic.main.fragment_main.buttonStorage
 
@@ -23,9 +22,6 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     buttonStorage.setOnClickListener {
       requireActivity().showFragment(StorageFragment())
     }
-    buttonPrivacyCenter.setOnClickListener {
-      showPrivacyCenter()
-    }
     buttonPrivacyFragment.setOnClickListener {
       requireActivity().showFragment(PrivacyFragment.newInstance())
     }
@@ -33,9 +29,5 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
   private fun showFetchSend() {
     requireActivity().showFragment(FetchSendFragment.newInstance())
-  }
-
-  private fun showPrivacyCenter() {
-    requireActivity().showFragment(PrivacyCenterFragment.newInstance())
   }
 }
